57f3f5f0ad821f2c644b9a1e29cda49a80585538
Juanfco0128 <Juanfco0128@gmail.com>
2019-11-30 14:19:42 +0000

    first commit

--- src/js/component/home.js
+++ src/js/component/home.js
@@ -1,24 +1,45 @@
-import React from "react";
-
-//include images into your bundle
-import rigoImage from "../../img/rigo-baby.jpg";
+import React, { useState } from "react";
 
 //create your first component
 export function Home() {
+	const [inputValue, setInputValue] = useState("data");
+	const [todos, setTodos] = useState([""]);
+
+	const removeTodo = todo => {
+		let newTodos = [...todos]; // duplicar arreglo
+		let index = todos.indexOf(todo); // encontrar la posicion (indice) del todo
+		newTodos.splice(index, 1); // quitar el elemnento del arreglo usando su indice
+		setTodos(newTodos); // actualizar el arreglo original todos
+	};
 	return (
-		<div className="text-center mt-5">
-			<h1>Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
+		<div>
+			<div className="text-center mt-5">
+				<h1> ToDo Lits</h1>
+				<input
+					style={{ position: "absolute", left: "45%" }}
+					onChange={e => setInputValue(e.target.value)}
+					onKeyPress={e => {
+						if (e.charCode === 13) {
+							setTodos(todos.concat(<h4>{inputValue}</h4>));
+						}
+					}}
+				/>
+				<div className="text-center list">
+					<ol>
+						{todos.map((t, i) => (
+							<li
+								key={i}
+								className="d-flex justify-content-between">
+								{t}
+								<button
+									className="Delete"
+									onClick={() => removeTodo(t)}
+								/>
+							</li>
+						))}
+					</ol>
+				</div>
+			</div>
 		</div>
 	);
 }
